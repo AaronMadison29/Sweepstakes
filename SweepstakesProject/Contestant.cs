a33e34dab5891df7862354fe9d355e1605b9a1a5
Aaron Madison <aaronmadison29@gmail.com>
2020-01-30 14:11:33 -0600

    Conestent class completed and UserInterface init

--- SweepstakesProject/Contestant.cs
+++ SweepstakesProject/Contestant.cs
@@ -8,5 +8,27 @@ namespace SweepstakesProject
 {
     class Contestant
     {
+        private string firstName;
+        private string lastName;
+        private string email;
+        public int regNumber;
+
+        public Contestant(string name, string email, int regNumber)
+        {
+            firstName = name.Split(' ')[0];
+            lastName = name.Split(' ')[1];
+            this.email = email;
+            this.regNumber = regNumber;
+        }
+
+        public string GetName()
+        {
+            return firstName + " " + lastName;
+        }
+
+        public string GetEmail()
+        {
+            return email;
+        }
     }
 }
